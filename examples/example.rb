d650206018562e1f78a40c119f5f1f3954bcb7ae
Dominic Wroblewski <domness@gmail.com>
2012-09-04 17:31:05 +1000

    Updates example file

--- examples/example.rb
+++ examples/example.rb
@@ -5,9 +5,10 @@ require "zohax"
 @token = "c9515d642d1557bf354f082c23e86a5f"
 
 crm = Zohax::Crm.new(@username, @password, @token)
-data = { "First Name" => "John", "Last Name" => "Doe", "Company" => "SomeCompany"}
+data = { "First Name" => "John", "Last Name" => "Doe", "Company" => "Some Company"}
 record_id = crm.add_lead(data)
-data = { "First Name" => "John", "Last Name" => "Doe", "Company" => "NEWCOMPANY"}
+# record_id = "661735000000055001"
+data = { "First Name" => "John", "Last Name" => "Doe", "Company" => "New Company"}
 crm.update_lead(data, record_id)
 
 puts record_id
