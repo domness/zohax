8ad0bd793471d73396321b3c8e6bc6531ad4dce1
Matt Farmer <matt@frmr.me>
2012-11-06 16:28:34 -0500

    Add setters for data fields.

--- lib/zohax/lead.rb
+++ lib/zohax/lead.rb
@@ -15,32 +15,64 @@ module Zohax
       @data["Lead Owner"]
     end
 
+    def lead_owner=(str)
+      @data["Lead Owner"] = str
+    end
+
     def company
       @data["Company"]
+    end
+
+    def company=(str)
+      @data["Company"] = str
     end
 
     def first_name
       @data["First Name"]
     end
 
+    def first_name=(str)
+      @data["First Name"] = str
+    end
+
     def last_name
       @data["Last Name"]
+    end
+
+    def last_name=(str)
+      @data["Last Name"] = str
     end
 
     def email
       @data["Email"]
     end
 
+    def email=(str)
+      @data["Email"] = str
+    end
+
     def phone
       @data["Phone"]
+    end
+
+    def phone=(str)
+      @data["Phone"] = str
     end
 
     def fax
       @data["Fax"]
     end
 
+    def fax=(str)
+      @data["Fax"] = str
+    end
+
     def mobile
-      @data["mobile"]
+      @data["Mobile"]
+    end
+
+    def mobile=(str)
+      @data["Mobile"] = str
     end
 
     def full_name
